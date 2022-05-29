b0c879d0257259dd73f97da3a21088d63b9fc024
adamhinton <adam.d.hinton@gmail.com>
2022-05-28 21:53:50 -0400

    take in setTodoList in form

--- src/components/AddNewTaskForm.js
+++ src/components/AddNewTaskForm.js
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 
 const AddNewTaskForm = (props) => {
+  const { setTodoList } = props;
+
   return (
     <StyledForm
       onSubmit={(e) => {
