78457dcfa8d89c7c20bb296fd7be72a5882943bb
adamhinton <adam.d.hinton@gmail.com>
2022-05-28 22:55:15 -0400

    pseudocode plan

---
+++ src/components/TodosContainer.js
@@ -0,0 +1,7 @@
+//completed true false
+// dynamic header
+// checkbox : just completed boolean
+// onClick:  just set completed to !completed. Update state
+// delete button should be same
+
+const TodosContainer = (props = {});
