cb5685c3546e09e8ebceeff1d7f2792b3fc49ee9
adamhinton <adam.d.hinton@gmail.com>
2022-05-29 17:23:11 -0400

    another file name typo

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import { render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer";
 
 test("[1] Header renders without errors", () => {
   render(<Header />);
@@ -13,4 +14,10 @@ test("[2] Header and Delete All button appear on screen", () => {
 
   expect(h1).toBeVisible();
   expect(deleteAllButton).toBeVisible();
+});
+
+test("[3] Matches snapshot from 5.29.22", () => {
+  const component = renderer.create(<Header />);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
