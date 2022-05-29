7df137b4339656c85087043ca0d7892159e2804b
adamhinton <adam.d.hinton@gmail.com>
2022-05-29 12:32:33 -0400

    make delete all button and style it

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ const Header = () => {
   return (
     <StyledHeader>
       <h1>Inventora Tasks</h1>
+      <StyledDeleteAllButton>Delete All</StyledDeleteAllButton>
     </StyledHeader>
   );
 };
@@ -17,4 +18,17 @@ const StyledHeader = styled.header`
   margin-left: 5%;
   border-bottom: 1px solid lightgrey;
   margin-bottom: 3%;
+  align-items: center;
+`;
+
+const StyledDeleteAllButton = styled.button`
+  border: none;
+  background-color: #0d98ba;
+  width: 150px;
+  color: white;
+  height: 50%;
+  margin-left: 200px;
+  font-size: 1.25rem;
+  padding: 15px 30px;
+  border-radius: 10px;
 `;
