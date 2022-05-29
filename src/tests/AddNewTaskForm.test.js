4df6006ddd931b6840f9146fabb4873650f80169
adamhinton <adam.d.hinton@gmail.com>
2022-05-29 17:26:40 -0400

    "[1] AddNewTaskForm renders without errors"

--- src/tests/AddNewTaskForm.test.js
+++ src/tests/AddNewTaskForm.test.js
@@ -1,3 +1,7 @@
 import AddNewTaskForm from "../components/AddNewTaskForm";
 import { render, screen } from "@testing-library/react";
 import renderer from "react-test-renderer";
+
+test("[1] AddNewTaskForm renders without errors", () => {
+  render(<AddNewTaskForm />);
+});
