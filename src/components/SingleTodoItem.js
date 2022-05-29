624d4789bc3a9caf6a804f3fbda62ad8f430bd74
adamhinton <adam.d.hinton@gmail.com>
2022-05-29 12:04:48 -0400

    delete c.logs

--- src/components/SingleTodoItem.js
+++ src/components/SingleTodoItem.js
@@ -1,6 +1,5 @@
 const SingleTodoitem = (props) => {
   const { completed, todoItem } = props;
-  console.log("blah blah blah");
   return (
     <li>
       <input type="checkbox" checked={completed}></input>
