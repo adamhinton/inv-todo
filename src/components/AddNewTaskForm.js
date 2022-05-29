df31b3b77b1db2b4d15cd050d30432cce7d9ed8a
adamhinton <adam.d.hinton@gmail.com>
2022-05-28 21:40:38 -0400

    import styled to ANTF

--- src/components/AddNewTaskForm.js
+++ src/components/AddNewTaskForm.js
@@ -1,7 +1,15 @@
 import React from "react";
+import styled from "styled-components";
 
 const AddNewTaskForm = (props) => {
-  return <form>fjdsiofsadijofsad</form>;
+  return (
+    <form>
+      <label>Add New Task</label>
+      <input placeholder="Enter Task Description..."></input>
+    </form>
+  );
 };
 
 export default AddNewTaskForm;
+
+const StyledForm = styled.form``;
