d12a68debc50df284530a9cf7b40f8d57534857a
adamhinton <adam.d.hinton@gmail.com>
2022-05-29 17:11:17 -0400

    add App snapshot testing

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,14 @@
 import App from "../App";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import renderer from "react-test-renderer";
 
 test("[1] App renders without errors", () => {
   render(<App />);
+});
+
+test("[2] Matches snapshot from 5.29.22", () => {
+  const component = renderer.create(<App />);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
