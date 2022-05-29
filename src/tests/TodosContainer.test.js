849cc46a288ccc7ac084ed6681685461be95a613
adamhinton <adam.d.hinton@gmail.com>
2022-05-29 17:57:38 -0400

    make fake todo list

--- src/tests/TodosContainer.test.js
+++ src/tests/TodosContainer.test.js
@@ -1,3 +1,15 @@
 import TodosContainer from "../components/TodosContainer";
 import { render, screen } from "@testing-library/react";
 import renderer from "react-test-renderer";
+
+const fakeTodoList = [
+  {
+    text: "Task one",
+    id: 1,
+    completed: false,
+  },
+];
+
+test("[1] TodosContainer renders without errors", () => {
+  render(<TodosContainer />);
+});
