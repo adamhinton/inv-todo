2812196fc6ebce9b965d6a2d00a5490e4c1320fc
adamhinton <adam.d.hinton@gmail.com>
2022-05-29 17:55:13 -0400

    "[4] Input text changes when user types"

--- src/tests/AddNewTaskForm.test.js
+++ src/tests/AddNewTaskForm.test.js
@@ -1,6 +1,7 @@
 import AddNewTaskForm from "../components/AddNewTaskForm";
 import { render, screen } from "@testing-library/react";
 import renderer from "react-test-renderer";
+import userEvent from "@testing-library/user-event";
 
 test("[1] AddNewTaskForm renders without errors", () => {
   render(<AddNewTaskForm />);
@@ -22,4 +23,14 @@ test("[3] Matches snapshot from 5.29.22", () => {
   const component = renderer.create(<AddNewTaskForm />);
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+test("[4] Input text changes when user types", () => {
+  render(<AddNewTaskForm />);
+  const addNewTaskInput = screen.getByTestId("add-new-task-input");
+
+  expect(addNewTaskInput).toHaveValue("");
+
+  userEvent.type(addNewTaskInput, "abc");
+  expect(addNewTaskInput).toHaveValue("abc");
 });
