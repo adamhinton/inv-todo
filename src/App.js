d3d543baf65abeacfc44da104560eeafd29ee9f7
adamhinton <adam.d.hinton@gmail.com>
2022-05-28 20:31:57 -0400

    basic universal styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
-      <Header />
+      <div className="container">
+        <Header />
+      </div>
     </div>
   );
 }
