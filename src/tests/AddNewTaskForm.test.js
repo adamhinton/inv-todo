71a73a0bc764177804eb019ea2b7ae9d8b854374
adamhinton <adam.d.hinton@gmail.com>
2022-05-29 17:16:09 -0400

    correct component name typo

--- src/tests/AddNewTaskForm.test.js
+++ src/tests/AddNewTaskForm.test.js
@@ -1,6 +1,15 @@
-import AddNewTaskForm from "../components/AddNewTaskForm";
+import Header from "../components/Header";
 import { render, screen } from "@testing-library/react";
 
-test("[1] AddNewTaskForm renders without errors", () => {
-  render(<AddNewTaskForm />);
+test("[1] Header renders without errors", () => {
+  render(<Header />);
+});
+
+test("[2] Header and Delete All button appear on screen", () => {
+  render(<Header />);
+
+  const h1 = screen.getByText("Inventora Tasks");
+  const deleteAllButton = screen.getAllByText("Delete All");
+
+  expect(h1).toBeVisible();
 });
