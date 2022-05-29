e50eb3a26840ad7b014e542663ec674a9b853f6c
adamhinton <adam.d.hinton@gmail.com>
2022-05-29 17:31:10 -0400

    "[2] Label, form field and add task button are visible"

--- src/tests/AddNewTaskForm.test.js
+++ src/tests/AddNewTaskForm.test.js
@@ -5,3 +5,15 @@ import renderer from "react-test-renderer";
 test("[1] AddNewTaskForm renders without errors", () => {
   render(<AddNewTaskForm />);
 });
+
+test("[2] Label, form field and add task button are visible", () => {
+  render(<AddNewTaskForm />);
+
+  const addNewTaskLabel = screen.getByText("Add New Task");
+  const addNewTaskInput = screen.getByTestId("add-new-task-input");
+  const addNewTaskButton = screen.getByText("+ Add Task");
+
+  expect(addNewTaskLabel).toBeVisible();
+  expect(addNewTaskInput).toBeVisible();
+  expect(addNewTaskButton).toBeVisible();
+});
