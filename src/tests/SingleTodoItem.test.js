63ab80ddfd7e90c712ddabb301c5ba4c6d016330
adamhinton <adam.d.hinton@gmail.com>
2022-05-29 20:45:41 -0400

    "[2] SingleTodoItem complete renders without errors"

--- src/tests/SingleTodoItem.test.js
+++ src/tests/SingleTodoItem.test.js
@@ -3,13 +3,23 @@ import { render, screen } from "@testing-library/react";
 import renderer from "react-test-renderer";
 
 const fakeTodoItemIncomplete = {
-  text: "Fake Todo Name",
+  text: "Fake Todo Name Incomplete",
   id: 1,
   completed: false,
+};
+
+const fakeTodoItemComplete = {
+  text: "Fake Todo Name Complete",
+  id: 2,
+  completed: true,
 };
 
 test("[1] SingleTodoItem incomplete renders without errors", () => {
   render(
     <SingleTodoitem completed={false} todoItem={fakeTodoItemIncomplete} />
   );
+});
+
+test("[2] SingleTodoItem complete renders without errors", () => {
+  render(<SingleTodoitem completed={true} todoItem={fakeTodoItemComplete} />);
 });
